f3e1fa89a445f38af24b6dc92f5a49268aeae9ac
ENOCH YEOM <hykqo@naver.com>
2023-01-31 13:01:20 +0900

    영수증 api호출 및  영수증 문자열 추출 기능 구현.

---
+++ app/src/main/java/kr/co/nicevan/nvcat/dto/ResponseDTO.java
@@ -0,0 +1,83 @@
+package kr.co.nicevan.nvcat.dto;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class ResponseDTO {
+
+    public static class ReceiptDTO {
+        @SerializedName("shopName")
+        @Expose
+        private String shopName; //매장명
+        @SerializedName("companyNo")
+        @Expose
+        private String companyNo; //사업자번호
+        @SerializedName("address")
+        @Expose
+        private String address; //주소
+        @SerializedName("owner")
+        @Expose
+        private String owner; //대표자
+        @SerializedName("tell")
+        @Expose
+        private String tell; //전화
+        @SerializedName("payDate")
+        private String payDate; //구매일
+        @SerializedName("items")
+        @Expose
+        private List<ItemDTO> items;
+        public String getShopName() {
+            return shopName;
+        }
+        public String getCompanyNo() {
+            return companyNo;
+        }
+        public String getAddress() {
+            return address;
+        }
+        public String getOwner() {
+            return owner;
+        }
+        public String getTell() {
+            return tell;
+        }
+        public String getPayDate() {
+            return payDate;
+        }
+        public List<ItemDTO> getItems() {
+            return items;
+        }
+
+        public class ItemDTO {
+            String itemName; //상품명(8자까지 표출)
+            int price; //구매가격
+            String unit; //g or 개
+            int quantity; //수량
+
+            public String getItemName() {
+                return itemName;
+            }
+            public int getPrice() {
+                return price;
+            }
+            public String getUnit() {
+                return unit;
+            }
+            public int getQuantity() {
+                return quantity;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/kr/co/nicevan/nvcat/service/ReceiptService.java
@@ -0,0 +1,15 @@
+package kr.co.nicevan.nvcat.service;
+
+
+import android.support.annotation.NonNull;
+
+import kr.co.nicevan.nvcat.dto.CardDTO;
+import kr.co.nicevan.nvcat.dto.RequestDTO;
+import kr.co.nicevan.nvcat.dto.ResponseDTO;
+import kr.co.nicevan.nvcat.retrofit.RevealStringCallbacks;
+
+public interface ReceiptService {
+    void printReceiptByOrder(RequestDTO.ReceiptDTO request, CardDTO card, @NonNull RevealStringCallbacks callbacks);
+    String outStringForReceipt(ResponseDTO.ReceiptDTO response, CardDTO card);
+    String formatterByLeftSpace(String inputString, int length);
+}
