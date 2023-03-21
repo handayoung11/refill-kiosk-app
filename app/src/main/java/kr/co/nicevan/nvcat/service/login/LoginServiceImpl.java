ca6a550a6aa84b32eca1b9567fefd012a4434c3f
ENOCH YEOM <hykqo@naver.com>
2023-03-21 17:48:38 +0900

    쿠키

--- app/src/main/java/kr/co/nicevan/nvcat/service/login/LoginServiceImpl.java
+++ app/src/main/java/kr/co/nicevan/nvcat/service/login/LoginServiceImpl.java
@@ -1,7 +1,16 @@
 package kr.co.nicevan.nvcat.service.login;
 
+import android.webkit.CookieManager;
+
+import java.net.CookieHandler;
+import java.util.List;
+
+import kr.co.nicevan.nvcat.CommonUtil;
 import kr.co.nicevan.nvcat.retrofit.RetrofitClient;
 import kr.co.nicevan.nvcat.retrofit.callback.LoginDefaultCallback;
+import okhttp3.Cookie;
+import okhttp3.HttpUrl;
+import okhttp3.JavaNetCookieJar;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,6 +31,14 @@ public class LoginServiceImpl implements LoginService {
             @Override
             public void onResponse(Call call, Response response) {
                 super.onResponse(call, response);
+                // cookie load
+                List<Cookie> cookies = new JavaNetCookieJar(CookieHandler.getDefault()).loadForRequest(HttpUrl.get(CommonUtil.BASE_URL));
+
+                // webview login을 위해 cookie 저장
+                for (Cookie cookie : cookies) {
+                    String cookieString = cookie.name() + "=" + cookie.value() + "; domain=" + cookie.domain();
+                    CookieManager.getInstance().setCookie(cookie.domain(), cookieString);
+                }
                 if (onSuccess != null)  {
                     onSuccess.run();
                 }
