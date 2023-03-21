1e939f0f607a92f2552a65d9970b018670520ada
ENOCH YEOM <hykqo@naver.com>
2023-03-21 17:49:19 +0900

    saveKioksOrders함수 기능 작성 - 결제 후 웹 통신 로직

--- app/src/main/java/kr/co/nicevan/nvcat/service/order/OrderService.java
+++ app/src/main/java/kr/co/nicevan/nvcat/service/order/OrderService.java
@@ -1,8 +1,11 @@
 package kr.co.nicevan.nvcat.service.order;
 
+import androidx.annotation.NonNull;
+
 import kr.co.nicevan.nvcat.dto.KioskOrderDTO;
+import kr.co.nicevan.nvcat.retrofit.RevealLongCallbacks;
 
 public interface OrderService {
 
-    void saveKioskOrders(KioskOrderDTO.SaveOrders saveOrders);
+    void saveKioskOrders(KioskOrderDTO.SaveOrders saveOrders, @NonNull RevealLongCallbacks callbacks);
 }
--- app/src/main/java/kr/co/nicevan/nvcat/service/order/OrderServiceImpl.java
+++ app/src/main/java/kr/co/nicevan/nvcat/service/order/OrderServiceImpl.java
@@ -2,12 +2,18 @@ package kr.co.nicevan.nvcat.service.order;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import kr.co.nicevan.nvcat.AppConfig;
 import kr.co.nicevan.nvcat.dto.KioskOrderDTO;
+import kr.co.nicevan.nvcat.dto.OrderDTO;
 import kr.co.nicevan.nvcat.retrofit.RetrofitClient;
+import kr.co.nicevan.nvcat.retrofit.RevealLongCallbacks;
+import kr.co.nicevan.nvcat.retrofit.error.ErrorCode;
+import kr.co.nicevan.nvcat.retrofit.error.ErrorResponse;
+import kr.co.nicevan.nvcat.retrofit.error.ErrorUtils;
 import kr.co.nicevan.nvcat.service.login.LoginService;
 import kr.co.nicevan.nvcat.util.KeyStoreUtil;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,26 +23,46 @@ public class OrderServiceImpl implements OrderService {
     LoginService loginService = AppConfig.getInstance().loginService();
 
     @Override
-    public void saveKioskOrders(KioskOrderDTO.SaveOrders saveOrders) {
+    public void saveKioskOrders(KioskOrderDTO.SaveOrders saveOrders, @NonNull RevealLongCallbacks callbacks) {
         KeyStoreUtil keyStoreUtil = KeyStoreUtil.getInstance();
-
         //login 실행 후 api 호출
         loginService.login(keyStoreUtil.getData("id", null), keyStoreUtil.getData("pw", null),
-                () -> saveKioskOrdersApi(saveOrders), null);
-
-    }
-
-    private void saveKioskOrdersApi(KioskOrderDTO.SaveOrders saveOrders) {
-        RetrofitClient.getDefaultResponseAPI().saveKioskOrders(saveOrders)
-                .enqueue(new Callback<ResponseBody>() {
+                () -> saveKioskOrdersApi(saveOrders, new RevealOrderRespCallbacks() {
                     @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        Log.d("결제", "결제 완료");
+                    public void onSuccess(@NonNull OrderDTO value) {
+                        Log.d("결제", "결제 완료, odIdx=[ "+value.getOdId()+" ]");
+                        callbacks.on(value.getOdId());
                     }
 
                     @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    public void onError(@NonNull ErrorResponse errorResponse) {
+                        Log.d("","==================================================");
+                        Log.d(this.getClass().getSimpleName(),"saveKioskOrders");
                         Log.d("결제", "결제 실패");
+                        Log.d("[error code] : ", String.valueOf(errorResponse.getStatus()));
+                        Log.d("[error title] : ", errorResponse.getTitle());
+                        Log.d("[error msg] : ", errorResponse.getMsg());
+                        Log.d("","==================================================");
+                    }
+                }), null);
+
+    }
+
+    private void saveKioskOrdersApi(KioskOrderDTO.SaveOrders saveOrders, @NonNull RevealOrderRespCallbacks callbacks) {
+        RetrofitClient.getDefaultResponseAPI()
+                .saveKioskOrders(saveOrders)
+                .enqueue(new Callback<OrderDTO>() {
+                    @Override
+                    public void onResponse(Call<OrderDTO> call, Response<OrderDTO> response) {
+                        // status code >= 200 || code < 300
+                        if(response.isSuccessful()) callbacks.onSuccess(response.body());
+                        // status code < 200 || code >= 300
+                        else callbacks.onError(ErrorUtils.parseError(response));
+                    }
+
+                    @Override
+                    public void onFailure(Call<OrderDTO> call, Throwable t) {
+                        callbacks.onError(ErrorResponse.of(ErrorCode.RETROFIT_NETWORK_FAILED));
                     }
                 });
     }
